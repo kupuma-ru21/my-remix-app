a3977842dfcc34663e2dfd6109409d055f808186
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-08-24 01:45:05 -0700

    chore: add memos

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,9 +1,11 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <p id="index-page">
       This is a demo for Remix.
       <br />
-      Check out <a href="https://remix.run">the docs at remix.run</a>.
+      <Link to="/memos">Memos</Link>
     </p>
   );
 }
